041c46c1082426b5ca714170f5c79c4556271a3b
wilsonalex79 <wilsonalex79@gmail.com>
2024-10-03 13:49:28 +0530

    Update Product.dart

--- lib/models/Product.dart
+++ lib/models/Product.dart
@@ -1,34 +1,28 @@
-class Product {
+class Product{
   int id;
   String name;
-  int price;
-  String unit;
+  String price;
 
-  // Constructor
   Product({
     required this.id,
     required this.name,
-    required this.price,
-    required this.unit,
+    required this.price
   });
 
+
   // Named constructor for creating an instance from a JSON map
-  factory Product.fromJson(Map<String, dynamic> json) {
-    return Product(
-      id: json['id'] ?? 0,    // Provide a default value (e.g., 0) if id is null
-      name: json['name'] ?? '', // Provide an empty string if name is null
-      price: json['price'] != null ? json['price'] as int : 0, // Default price to 0 if null
-      unit: json['unit'] ?? '', // Provide an empty string if unit is null
-    );
-  }
+  Product.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        price = json['price'];
 
   // Method to convert an instance to a JSON map
   Map<String, dynamic> toJson() {
     return {
       'id': id,
       'name': name,
-      'price': price,
-      'unit': unit,
+      'price': price
     };
   }
+
 }
